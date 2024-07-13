fa1fffe01d727e0b0684feb4f7f36aafe38f858a
Jelle Meeus <sogladev@gmail.com>
2024-07-13 19:44:01 +0200

    add config

--- src/mod_reset_raid_cooldowns_loader.cpp
+++ src/mod_reset_raid_cooldowns_loader.cpp
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3
  */
+#include "Config.h"
 
 // From SC
 void AddSC_reset_raid_cooldowns_spell_script();
@@ -10,6 +11,8 @@ void AddSC_reset_raid_cooldowns_spell_script();
 // additionally replace all '-' in the module folder name with '_' here
 void Addmod_reset_raid_cooldownsScripts()
 {
+    if (!sConfigMgr->GetOption<bool>("ResetRaidCooldowns.Enable", true))
+        return;
     AddSC_reset_raid_cooldowns_spell_script();
 }
 
