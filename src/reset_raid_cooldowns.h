d016036fe2cbb501ea1085cbb875fe97561164b8
Jelle Meeus <sogladev@gmail.com>
2024-07-15 01:01:05 +0200

    additional configuration for cpp and update readme (#5)
    * smaller case
    
    * update
    
    * unordered_map to map
    
    * using instead of typedef
    
    * get rid of typedef
    
    * add const
    
    * update readme

--- src/reset_raid_cooldowns.h
+++ src/reset_raid_cooldowns.h
@@ -6,6 +6,12 @@
 #include "Config.h"
 #include "ScriptMgr.h"
 
+enum ResetRaidCooldownsSpells
+{
+    SPELL_EXHAUSTION = 57723,
+    SPELL_SATED = 57724
+};
+
 struct Id1Id2Key
 {
     uint32 id1;
@@ -17,30 +23,33 @@ struct Id1Id2Key
     }
 };
 
-typedef Id1Id2Key InstanceIdBossIdKey;
-typedef Id1Id2Key MapIdBossIdKey;
+using MapIdBossIdKey = Id1Id2Key;
+using InstanceIdBossIdKey = Id1Id2Key;
 
 class ResetRaidCooldowns
 {
 public:
     static ResetRaidCooldowns* instance();
 
-    bool IsEnabled{false};
-    uint32 CombatTimeRequiredInSeconds;
+    bool doResetArenaSpells;
+    bool doResetPetCooldowns;
+    bool isEnabled{false};
+    std::vector<uint32> categories;
+    std::vector<uint32> spells;
+    uint32 combatTimeRequiredInSeconds;
 
+    std::map<uint32, bool> mapIdEnableSettings;
+    std::map<MapIdBossIdKey, bool> mapIdBossIdDisableSettings;
+    std::map<InstanceIdBossIdKey, uint32> instanceIdBossIdCombatStartedTimeMap;
+
+    void LoadSpellsSettings(std::string const& spellsSettingsString);
+    void LoadCategoriesSettings(std::string const& categoriesSettingsString);
     void LoadMapIdEnableSettings(std::string const& mapIdEnableSettingsString);
     void LoadMapIdBossIdDisableSettings(std::string const& mapIdBossIdDisableSettingsString);
 
-    bool IsMapBossEnabled(uint32 mapId, uint32 bossId);
+    bool IsMapBossEnabled(uint32 mapId, uint32 bossId) const;
     void SetCombatStarted(uint32 instanceId, uint32 bossId);
     uint32 GetCombatStartedTime(uint32 instanceId, uint32 bossId) const;
-
-    typedef std::map<uint32, bool> MapIdEnableSettings;
-    MapIdEnableSettings mapIdEnableSettings;
-    typedef std::map<MapIdBossIdKey, bool> MapIdBossIdDisableSettings;
-    MapIdBossIdDisableSettings mapIdBossIdDisableSettings;
-    typedef std::map<InstanceIdBossIdKey, uint32> InstanceIdBossIdCombatStartedTimeMap;
-    InstanceIdBossIdCombatStartedTimeMap instanceIdBossIdCombatStartedTimeMap;
 };
 
 #define sResetRaidCooldowns ResetRaidCooldowns::instance()
